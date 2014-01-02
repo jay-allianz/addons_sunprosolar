936bb22b9023efb9e0b9e5d37e7e73f6ff451533
Husen <husen@Husen.(none)>
2014-01-02 19:21:21 +0530

    Commit project flow change invoice change and other workflow modifications

---
+++ sunprosolar/sps_project/__openerp__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2012 Allianz Technology, A subsidiary of SAT Group, Inc.
+#    Copyright (C) 2012 OpenERP SA (<http://www.serpentcs.com>)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    "name" : "SPS - Project",
+    "version" : "6.3",
+    "author" : "Allianz Technology",
+    "category" : "Tools",
+    "website" : "'http://www.allianztechnology.com",
+    "description": """This module provides the functionality to generate Invoice for customer. 
+    """,
+    'data' : [
+        'data/project_task_type_data.xml',
+    ],
+    'depends': ['project'],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ sunprosolar/sps_sale/wizard/sale_make_invoice_advance.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 OpenERP SA (<http://www.openerp.com>)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>
+#
+##############################################################################
+
+from openerp.osv import fields, osv
+from openerp.tools.translate import _
+import openerp.addons.decimal_precision as dp
+
+class sale_advance_payment_inv(osv.osv_memory):
+    _inherit = "sale.advance.payment.inv"
+
+    _columns = {
+        'advance_payment_method':fields.selection(
+            [('all', 'Invoice the whole sales order')], required=True),
+    }
